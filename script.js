c052379cdc5eece1ae4bd30e9f9f5e4c1dcefd5c
saksshham <81289215+saksshham@users.noreply.github.com>
2021-07-13 22:06:55 +0530

    Version 1

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const gmail = document.querySelector('.gmail');
+gmail.addEventListener('click', (e) =>{
+    alert("Yo! Here's the Email: sakshammchaturvedii@gmail.com :)")
+})
+
 window.onload =function(){
     window.scrollTo(0, 10);
 };
